46912a20fa50615b32e019d3013ec46a5429668c
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2024-03-01 23:17:25 +0000

    Remove /pronouns command since it was only there for testing and pronouns can now be viewed via the pronouns screen

--- src/main/java/net/pneumono/pronouns/Pronouns.java
+++ src/main/java/net/pneumono/pronouns/Pronouns.java
@@ -1,26 +1,18 @@
 package net.pneumono.pronouns;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.pneumono.pneumonocore.config.Configs;
 import net.pneumono.pronouns.network.InformPronounsC2SPacket;
 import net.pneumono.pronouns.pronouns.PlayerPronouns;
-import net.pneumono.pronouns.pronouns.PronounsApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
-import static net.minecraft.server.command.CommandManager.argument;
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class Pronouns implements ModInitializer {
 	public static final String MOD_ID = "pronouns";
@@ -38,20 +30,5 @@ public class Pronouns implements ModInitializer {
 
 		ServerPlayNetworking.registerGlobalReceiver(INFORM_PRONOUNS_ID, InformPronounsC2SPacket::receive);
 		ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> Pronouns.uuidPronounsMap.remove(handler.getPlayer().getUuid()));
-
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
-			dispatcher.register(literal("pronouns")
-				.requires(source -> source.hasPermissionLevel(4))
-				.then(argument("player", EntityArgumentType.player())
-					.executes(context -> {
-						ServerPlayerEntity player = EntityArgumentType.getPlayer(context, "player");
-						PlayerPronouns pronouns = PronounsApi.getPlayerPronouns(player);
-						context.getSource().sendMessage(PronounsApi.getTranslatableTextWithPronouns("pronouns.test_message", pronouns, player.getName().getString()).formatted(Formatting.GOLD));
-
-						return 1;
-					})
-				)
-			)
-		);
 	}
 }
